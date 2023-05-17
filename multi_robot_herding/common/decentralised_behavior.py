cdb2f9eee80d841e406e2ae29ff2ff321c2365cb
dkhoanguyen <khoanguyendacdang2198@gmail.com>
2023-05-17 17:27:25 +1000

    Preparation for decentralised approach

--- multi_robot_herding/common/decentralised_behavior.py
+++ multi_robot_herding/common/decentralised_behavior.py
@@ -1,7 +1,25 @@
 # !/usr/bin/python3
 
+import numpy as np
 from multi_robot_herding.common.behavior import Behavior
+
+from multi_robot_herding.entity.entity import Autonomous
+from multi_robot_herding.entity.herd import Herd
+
 
 class DecentralisedBehavior(Behavior):
     def __init__(self):
         super(DecentralisedBehavior, self).__init__()
+        self._herds = []
+
+    def add_herd(self, herd: Herd):
+        self._herds.append(herd)
+
+    def extract_agent_states(self, agents: list):
+        agent: Autonomous
+        states = np.array([]).reshape((0, 4))
+        for agent in agents:
+            # Grab and put all poses into a matrix
+            states = np.vstack(
+                (states, np.hstack((agent.pose, agent.velocity, agent.acceleration))))
+        return agents
